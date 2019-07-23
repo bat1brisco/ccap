e2be64e7077de6dd7a8a7bb44450bf3ae411b2ed
ralph8892 <ralphbrufal@gmail.com>
2019-07-22 19:20:31 -0700

    updated links

--- application/views/transactions/index.php
+++ application/views/transactions/index.php
@@ -2,14 +2,14 @@
 	<div class="row">
 		<div class="col-md admin-sidebar pt-5 pl-0 pr-0">
 
-			<a class="nav-link rounded-0 link-active mb-1" href="<?php echo base_url(); ?>admindashboard">My Dashboard</a>
+			<a class="nav-link rounded-0 mb-1" href="<?php echo base_url(); ?>admindashboard">My Dashboard</a>
 			<a class="nav-link rounded-0 mb-1" href="<?php echo base_url(); ?>adminprofile">My Profile</a>
 			<a class="nav-link rounded-0 mb-1" href="<?php echo base_url(); ?>chat1">Messages</a>
 			<a class="nav-link rounded-0 mb-1" href="<?php echo base_url(); ?>users/manage_account">Manage Registration</a>
 			<a class="nav-link rounded-0 mb-1" href="<?php echo base_url(); ?>Carslisting/manage_cars">Manage Car Posts</a>
 			<a class="nav-link rounded-0 mb-1" href="<?php echo base_url(); ?>Partslisting/manage_parts">Manage Parts Posts</a>
 			<a class="nav-link rounded-0 mb-1" href="<?php echo base_url(); ?>users/administer_accounts">Users List</a>
-			<a class="nav-link rounded-0 mb-1" href="<?php echo base_url(); ?>transactions">Transactions</a>
+			<a class="nav-link rounded-0 link-active mb-1" href="<?php echo base_url(); ?>transactions">Transactions</a>
 			
 		</div>
 
